0f8d129f34c8c09ba9f84ff4dd89c1a6b412abae
Sergey Dudo <zamirdruazbu@gmail.com>
2025-09-11 21:48:00 +0300

    Lab1.3 Display video in window. Explore VideoCapture methods and test different formats: resolutions and color spaces.

--- lab1/1.py
+++ lab1/1.py
@@ -1,6 +1,25 @@
 import cv2
-img1 = cv2.imread(r'C:\Users\123\Desktop\acom\l1\lbo4tBsSzHE.jpg',cv2.WINDOW_NORMAL """ cv2.IMREAD_REDUCED_GRAYSCALE_8, cv2.IMREAD_GRAYSCALE , cv2.IMREAD_REDUCED_COLOR_8 """)
-cv2.namedWindow('Sabaka v kepke', cv2.WINDOW_NORMAL)
-cv2.imshow('Sabaka v kepke', img1)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def read():
+    video = cv2.VideoCapture(0)
+    video.set(cv2.CAP_PROP_BRIGHTNESS, 0.27)
+    video.set(cv2.CAP_PROP_FRAME_WIDTH, 140)
+    video.set(cv2.CAP_PROP_FRAME_HEIGHT, 50)
+    video.set(cv2.CAP_PROP_SATURATION, 0.5)
+    video.set(cv2.CAP_PROP_CONTRAST, 0.5)
+    ok, img = video.read()
+    w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH) )
+    h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT) )
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    video_writer = cv2.VideoWriter("output.avi", fourcc, 25, (w, h))
+    while (True):
+        ok, img = video.read()
+        cv2.imshow('CAM', img)
+        video_writer.write(img)
+        if cv2.waitKey(1) & 0xFF == ord('z'):
+            break
+
+    video.release()
+    video_writer.release()
+    cv2.destroyAllWindows()
+
+read()
